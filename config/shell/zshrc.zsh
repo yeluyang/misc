26d034934d91f17c068d91e5c1d0bc51f10917eb
yeluyang <ylycpg@gmail.com>
2020-10-16 10:38:40 +0800

    feat[config]: add shell

---
+++ config/shell/zshrc.zsh
@@ -0,0 +1,18 @@
+# setup custom env
+
+export GOPATH=${HOME} && export GOBIN=${GOPATH}/bin && export GOPROXY="https://goproxy.io" && export GO111MODULE="auto"
+export RUSTUP_DIST_SERVER="https://mirrors.tuna.tsinghua.edu.cn/rustup" && export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+
+# define custom command
+
+# proxy
+# local proxy server
+function ss_client_start() {
+        sudo sslocal -c /etc/shadowsocks.json -d start
+}
+function ss_client_stop() {
+        sudo sslocal -c /etc/shadowsocks.json -d stop
+}
+# terminal proxy
+alias proxy="export http_proxy=http://127.0.0.1:1080; export https_proxy=https://127.0.0.1:1080"
+alias unproxy="unset http_proxy; unset https_proxy"
