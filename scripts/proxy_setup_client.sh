5f52d9fe3f9e55313fb9e310e58b9df219b4d8c2
yeluyang <ylycpg@gmail.com>
2020-12-09 15:45:02 +0800

    code[scripts]: update proxy_setup

--- scripts/proxy_setup_client.sh
+++ scripts/proxy_setup_client.sh
@@ -27,10 +27,24 @@ server_type = 5
 server_port = 1086
 EOT
 
+command -v connect-proxy >/dev/null || apt install connect-proxy
+
 cat << EOT
 setup successfully. now please do following things by manually:
-    1. edit your own shadowsocks config from '/etc/shadowsocks/example.json'
-    2. run command 'sudo sslocal -c '/etc/shadowsocks/<your_config>.json -d start'
-now, you can run 'tsocks ssh username@server.ip' to access without jump server.
-fxxk jump server.
+
+  1. edit your own shadowsocks config from '/etc/shadowsocks/example.json'
+  2. run command 'sudo sslocal -c '/etc/shadowsocks/<your_config>.json -d start'
+
+now, you can use two method to access without jump server:
+
+  1. run 'tsocks ssh username@any.server.ip' to access any server without any configure
+  2. config specifed hosts which should access with proxy:
+     2.1 write following content in your '.ssh/config':
+
+         Host example.*
+          ProxyCommand connect -S 127.0.0.1:1086 %h %p
+
+     2.2 run 'ssh username@example.server.com' to access hosts under domain 'example.*' directly
+
+fxxk the jump server!
 EOT
