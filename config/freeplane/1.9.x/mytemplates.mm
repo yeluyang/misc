d76cbcc784677b9cd84f6183f2b78bc72c69c2ce
yeluyang <ylycpg@gmail.com>
2021-10-26 20:35:51 +0800

    [config]: update freeplane 1.9.x

---
+++ config/freeplane/1.9.x/mytemplates.mm
@@ -0,0 +1,186 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="topic" ID="ID_348761334"><hook NAME="MapStyle">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="default" LAST="false">
+            <node_leaf_condition/>
+        </conditional_style>
+    </conditional_styles>
+    <properties fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="4 pt">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1523236075" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork" BORDER_WIDTH_LIKE_EDGE="true" MAX_WIDTH="255 cm" VGAP_QUANTITY="4 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1523236075" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="12" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" VGAP_QUANTITY="4 pt"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" VGAP_QUANTITY="4 pt">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT" VGAP_QUANTITY="4 pt"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" VGAP_QUANTITY="4 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#ccffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#002080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="4 pt">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork" VGAP_QUANTITY="4 pt">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork" VGAP_QUANTITY="4 pt">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" VGAP_QUANTITY="4 pt">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" VGAP_QUANTITY="4 pt">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble" VGAP_QUANTITY="4 pt">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" VGAP_QUANTITY="8 pt" COLOR="#111111" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="4 pt" UNIFORM_SHAPE="true" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="20"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" VGAP_QUANTITY="8 pt" COLOR="#111111" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" VGAP_QUANTITY="8 pt" COLOR="#111111" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" VGAP_QUANTITY="8 pt" COLOR="#111111" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" VGAP_QUANTITY="8 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,12" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,13" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,14" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,15" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,16" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,17" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,18" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,19" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,20" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,21" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,22" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,23" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,24" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,25" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,26" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,27" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,28" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,29" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,30" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,31" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,32" STYLE="as_parent" SHAPE_HORIZONTAL_MARGIN="4 pt" SHAPE_VERTICAL_MARGIN="1 pt" MAX_WIDTH="20 cm" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="8 pt">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="FOR_BRANCHES"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="HEADINGS"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+</node>
+</map>
